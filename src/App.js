94af3efaa3738550d3508faea78ce5bd3c91df68
TylerLadas <tylerladas@hotmail.com>
2021-10-01 11:52:33 -0400

    add about component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 
+
 function App() {
   return (
     <div>
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import tyler from "../../assets/about/tyler.jpg"
+
 
 function About() {
   return (
     <section>
       <h1 id="about">About Me</h1>
+      <img src={tyler} alt="Tyler Ladas" />
+      <p>
+        Hi, I'm Tyler. I am an aspiring web developer. I live in Ottawa, Ontario Canada with my wife and daughter. 
+        Previously, I worked in healthcare as a Anesthesia Assistant and Respiratory Therapist.
+        I am ready for a change and to begin the next exciting chapter of my life. Becoming a web developer will allow 
+        me to combine skills and passions in order to bring projects to life in a dynamic, visually appealing medium. 
+        Some of my other interests include cooking and gardening. I am excited for what the future holds!
+      </p>
     </section>
   );
 }
